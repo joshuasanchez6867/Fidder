e0e5a59e076d9bce4d8515d9b04c133e3296a4e4
Tiani Edwards <tianiedwards@Tianis-MacBook-Pro.local>
2023-10-06 11:19:49 -0500

    Setting started with the Pantry list

--- src/Components/Pantry.tsx
+++ src/Components/Pantry.tsx
@@ -1,16 +1,27 @@
+import { Container, Row, Col } from 'react-bootstrap';
 import styles from './Pantry.module.css';
-
+//import { MDBContainer, MDBRow, MDBCol } from 'mdb-react-ui-kit';
 const Pantry: React.FC<{}> =  () => {
     return(
-        <>
-        <div className="row">
-            <div className={styles['col-3']}>
-                <main id='ingredients_page' className="q.layout">
-                    helof
-                </main>
-            </div>
-        </div>
-        </>
+    <Container fluid>
+      <Row>
+        <Col className={styles['col-4']} sm={4}>
+            <header className={styles['header-block']} >
+               <div className={styles['head-navigation']} >
+                <div className={styles['head-nav-center']}>
+                    <h1 className={styles['head-title']}>Pantry</h1>
+                    <div className={styles['head-sub-title']}>You have 0 ingredients</div>
+                </div>
+                <div className={styles['head-nav-right']}></div>
+                </div> 
+                <div className={styles['search-bar wrap w-form']} >
+
+                </div>
+            </header>
+        </Col>
+        <Col className={styles['col-8']} sm={8}>sm=8</Col>
+      </Row>
+    </Container>
     );
 
 }
